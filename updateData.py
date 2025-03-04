c8f3db348c97a9896f6811fae2577b68a7ac54ab
Joon <joonhyoo12@gmail.com>
2025-03-04 23:46:38 +1100

    added python script to update counts

---
+++ updateData.py
@@ -0,0 +1,58 @@
+import json
+from datetime import datetime
+
+today = datetime.today().strftime('%Y-%m-%d')\
+
+completed_task_msg = """What did you do today?
+(you can write more than one by adding a space)
+    l: lessons
+    r: review session
+    a: additional study
+    x: reset score
+"""
+
+with open('./src/data.json') as jsonData:
+    data = json.load(jsonData)
+
+
+def main():
+    tasks = input(completed_task_msg).split(' ')
+    for task in tasks:
+        handle_completed_task(task)
+
+    with open('./src/data.json', 'w') as jsonData:
+        json.dump(data, jsonData)
+
+
+def handle_completed_task(task):
+    match task:
+        case 'l':
+            print('lesson')
+            add_points(2)
+        case 'r':
+            print('review')
+            add_points(1)
+        case 'a':
+            print('additional study')
+            study_time = input('how many hours of study? ')
+            add_points(int(study_time))
+        case 'x':
+            print('reset score')
+            reset_points()
+        case _:
+            print('invalid input')
+
+
+def add_points(num):
+    for x in data:
+        if (x['date'] == today):
+            x['count'] += num
+
+
+def reset_points():
+    for x in data:
+        if (x['date'] == today):
+            x['count'] = 0
+
+
+main()
