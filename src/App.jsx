365d7e44b1ecc2891dd47d1bb87ad2a5d9a01d75
Joon <joonhyoo12@gmail.com>
2025-03-05 18:39:57 +1100

    implemented additional information on hover (very simple for now)

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import CalendarHeatmap from 'react-calendar-heatmap';
 import './styles.css';
 import learningData from './data.json';
 import { Tooltip } from 'react-tooltip';
+import { useState } from 'react';
 
 function App() {
+  const [selected, setSelected] = useState(null);
+
+  const sumValues = (obj) =>
+    Object.values(obj)
+      .map((num) => Number(num))
+      .reduce((a, b) => a + b, 0);
+
   const getOrdinalSuffix = (day) => {
     if (day > 3 && day < 21) return 'th'; // Covers 11-19
     switch (day % 10) {
@@ -28,14 +36,19 @@ function App() {
           if (!value) {
             return 'color-empty';
           }
-          return `color-${value.count}`;
+          return `color-${
+            sumValues(value['additional']) + value['studied'].length
+          }`;
         }}
         tooltipDataAttrs={(value) => {
           const date = new Date(value.date);
           const day = date.getDate();
           const month = date.toLocaleString('en-us', { month: 'long' }); // Full month name
-          const content = `${value.count} point${
-            value.count !== 1 ? 's' : ''
+          const points =
+            sumValues(value['additional']) + value['studied'].length;
+
+          const content = `${points} point${
+            points !== 1 ? 's' : ''
           } on ${month} ${day}${getOrdinalSuffix(day)}`;
           return {
             'data-tooltip-id': 'calendar-tooltip',
@@ -43,8 +56,42 @@ function App() {
           };
         }}
         values={learningData ? learningData : []}
+        onMouseOver={(e, selected_date) => setSelected(selected_date)}
       />
       <Tooltip id="calendar-tooltip" />
+      {selected && (
+        <>
+          {selected.date} {selected.count}
+          <div>
+            Bunpro lessons:
+            {selected.studied
+              .filter((item) => item === 'b')
+              .map(() => '✅')
+              .join('') || '❌'}
+          </div>
+          <div>
+            WaniKani lessons:
+            {selected.studied
+              .filter((item) => item === 'w')
+              .map(() => '✅')
+              .join('') || '❌'}
+          </div>
+          <div>
+            Review sessions:
+            {selected.studied
+              .filter((item) => item === 'r')
+              .map(() => '✅')
+              .join('') || '❌'}
+          </div>
+          <div>
+            Additional study:
+            {Object.entries(selected.additional)
+              .map(([key, value]) => `${key} for ${value} hrs`)
+              .join('') || '❌'}
+          </div>
+          <div>WaniKani lessons: ❌</div>
+        </>
+      )}
     </div>
   );
 }
